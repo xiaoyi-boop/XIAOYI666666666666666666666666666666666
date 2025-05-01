ae0cc74789f0c49a8e0ec05c91e187689b52a1df
xiaoyi-boop <1701291983@qq.com>
2025-05-01 21:05:01 +0800

    Add files via upload

---
+++ YIID.lua
@@ -0,0 +1,55 @@
+if game.PlaceId == 6839171747 then--doors
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/ID/refs/heads/main/%E4%BE%9D%E8%84%9A%E6%9C%ACdoors.lua",true))()
+elseif game.PlaceId == 3623096087 then--力量传奇
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/ID/refs/heads/main/%E4%BE%9D%E8%84%9A%E6%9C%AC%E5%8A%9B%E9%87%8F%E4%BC%A0%E5%A5%87.lua",true))()
+elseif game.PlaceId == 7239319209 then--Ohio
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/ID/refs/heads/main/%E4%BE%9D%E8%84%9A%E6%9C%ACohio.lua",true))()
+elseif game.PlaceId == 4618049391 then--河北唐县
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/ID/refs/heads/main/%E4%BE%9D%E8%84%9A%E6%9C%AC%E6%B2%B3%E5%8C%97%E5%94%90%E5%8E%BF.lua",true))()
+elseif game.PlaceId == 18241512421 then-- 狗熊岭危机
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("输入链接",true))()
+elseif game.PlaceId == 5256165620 then-- 火箭发射模拟器
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/ID/refs/heads/main/%E4%BE%9D%E8%84%9A%E6%9C%AC%E7%81%AB%E7%AE%AD%E5%8F%91%E5%B0%84.lua",true))()
+elseif game.PlaceId == 12931609417 then-- 颜色或死亡
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("输入链接",true))()
+elseif game.PlaceId == 110333320616502 then-- 格蕾丝
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("输入链接",true))()
+elseif game.PlaceId == 138837502355157 then-- 格蕾丝
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("输入链接",true))()
+elseif game.PlaceId == 13822889 then-- 伐木大亨2
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wumingjiaoben/ui/refs/heads/main/%E4%BC%90%E6%9C%A8%E5%A4%A7%E4%BA%A8(2).lua"))()
+elseif game.PlaceId == 12196278347 then--Refinery Caves
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("输入链接",true))()
+elseif game.PlaceId == 2753915549 then--BF
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("输入链接",true))()
+elseif game.PlaceId == 3101667897 then--极速传奇
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/ID/refs/heads/main/%E4%BE%9D%E8%84%9A%E6%9C%AC%E6%9E%81%E9%80%9F%E4%BC%A0%E5%A5%87.lua",true))()
+elseif game.PlaceId == 6516141723 then--doors
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("输入链接",true))()
+elseif game.PlaceId == 5041144419 then--scp
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/ID/refs/heads/main/%E4%BE%9D%E8%84%9A%E6%9C%ACSCP%E8%A7%92%E8%89%B2%E6%89%AE%E6%BC%94.lua",true))()
+elseif game.PlaceId == 5256165620 then--火箭
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("输入链接",true))()
+elseif game.PlaceId == 155615604 then--监狱人生
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/ID/refs/heads/main/%E4%BE%9D%E8%84%9A%E6%9C%AC%E7%9B%91%E7%8B%B1%E4%BA%BA%E7%94%9F.lua",true))()
+else--通用
+    YIJIAOBEN = "YIJIAOBEN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoyi-boop/ID/refs/heads/main/%E4%BE%9D%E8%84%9A%E6%9C%AC%E9%80%9A%E7%94%A8%E8%84%9A%E6%9C%AC.lua",true))()
+end
